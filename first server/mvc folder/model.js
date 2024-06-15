c98d97ac8b74ff6e8192acf3ac6a0e9238c23776
Rajan <rajan.rana6@gmail.com>
2024-06-15 09:34:03 +0530

    model of use html file

--- first server/mvc folder/model.js
+++ first server/mvc folder/model.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("this is home page");
+  res.sendfile(path.join(__filename + "/index.html"));
 });
 
 app.get("/about", (req, res) => {
@@ -15,6 +15,9 @@ app.get("/contact", (req, res) => {
   res.send("this is contact page");
 });
 
+app.get("/books/:bookName", (req, res) => {
+  res.send("you are looking for the book " + req.params.bookName);
+});
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
