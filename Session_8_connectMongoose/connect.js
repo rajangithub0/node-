422659be432333f999059cfe2bdb5be528ef2311
Rajan <rajan.rana6@gmail.com>
2024-07-06 09:33:42 +0530

    add how to connect datadase mongodb altes

--- Session_8_connectMongoose/connect.js
+++ Session_8_connectMongoose/connect.js
@@ -3,8 +3,8 @@ const mongoose =require("mongoose")
 async function connectDB(){
 
     try {
-        // await mongoose.connect('mongodb://localhost:27017/skillacademy');
-        await mongoose.connect('mongodb+srv://rajanrana6:fANyS5DZxGbTpiZE@cluster0.ne4pcyr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        await mongoose.connect('mongodb://localhost:27017/skillacademy');
+        //await mongoose.connect('url connect with mongodb altes')
         console.log("Connect DB");
     } catch (error) {
         console.log("error",error);
