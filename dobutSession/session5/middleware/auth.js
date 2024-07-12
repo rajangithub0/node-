740dcdbcdfd60f113cbbbe7a83af84231ef700cf
Rajan <rajan.rana6@gmail.com>
2024-07-12 19:13:16 +0530

    making the api login and register

--- dobutSession/session5/middleware/auth.js
+++ dobutSession/session5/middleware/auth.js
@@ -1,6 +1,18 @@
+const jwt=require('jsonwebtoken')
+
 const auth = (req,res,next)=>{
-    console.log(req.headers);
-    //const token=
+    //console.log(req.headers);
+    const token=req.headers.authorization;
+    console.log(token);
+    if(token){
+        const ogToken=token.split(' ')[1];
+        const data=jwt.verify(ogToken,process.env.JWT_SECRET);
+        console.log(data);
+        req.user=data //data will send the profile data
+        next();
+    }else{
+        return res.send("login first")
+    }
 }
 
 module.exports=auth
--- dobutSession/session5/routes/userRoute.js
+++ dobutSession/session5/routes/userRoute.js
@@ -5,35 +5,37 @@ const jwt=require("jsonwebtoken");
 const auth = require('../middleware/auth');
 const router =express.Router()
 
-router.post("/register",async(req,res)=>{
+router.post('/register',async(req,res)=>{
     try {
-        const {name,username,password,email}=req.body;
-        if(!name||!username|| !password ||email){
-            return res.status(400).send({massage:"enter all details"})
+        const {name,email,username,password}=req.body;
+        if(!name || !email || !username || !password){
+            return res.status(400).send({message:'All fields are mandatory'})
         }
-        let user=await UserModel.findOne((email))
+        let user= await UserModel.findOne({email});
         if(user){
-            return res.status(400).send({massage:"email is already use"})
+            return res.status(400).send({message:'Email Id Already registered'})
         }
-        user=await UserModel.findOne((username))
+        user= await UserModel.findOne({username});
         if(user){
-            return res.status(400).send({massage:"user name is already use"})
+            return res.status(400).send({message:'Username is already taken by Someone'})
         }
-        const hashPwd=await bcrypt.hash(password,10);
-        const newUser=new UserModel({name,email,username,password:hashPwd})
+        const hashPwd= await bcrypt.hash(password,10);
+        const newUser= new UserModel({name,email,username,password:hashPwd});
         const resp= await newUser.save();
-        return res.status(201).send({massage:"user created successfully"})
+        res.status(201).send({message:'User Registered Successfully'})
     } catch (error) {
-        res.status(500).send({massage:"internal error occur",error})
+        console.log(error);
+        res.status(500).send({message:'Internal error occured'})
     }
 })
-router.post('login',async (req,res)=>{
+
+router.post('/login',async (req,res)=>{
     try {
-        const {name,password}=req.body
+        const {email,password}=req.body
         if(!email||!password){
             return res.status(400).send({massage:"enter all details"})            
         }               
-        let user =await UserModel.findOne(email)
+        let user =await UserModel.findOne({email})
         if(!user){
             return res.status(400).send({message:'This Email Id is not registered with US'})
         }
@@ -41,18 +43,20 @@ router.post('login',async (req,res)=>{
         const match= await bcrypt.compare(password,user.password);
         if(match){
             const payload= {id:user._id,name:user.name, email:user.email};
-            const token= await jwt.sign(payload,process.env.JWT_SECRET);
+            const token= await jwt.sign(payload, process.env.JWT_SECRET);
             return res.status(200).send({message:'Logged In Successfully',token})
         }else{
             return res.status(500).send({message:'Invalid credentials'})
         }
     } catch (error) {
+        console.log(error)
         res.status(500).send({message:'Internal error occured'}) 
     }
 })
 
 router.get("/profile",auth,async(req,res)=>{
-
+    const usdata = req.user 
+    res.send({massage:'welcome to your profile',usdata})
 })
 
 module.exports=router
