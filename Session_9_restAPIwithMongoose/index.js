72c1ec29555d0ad154b89f0248fa1d799850f50c
Rajan <rajan.rana6@gmail.com>
2024-08-22 11:21:19 +0530

    package intsall cors

--- Session_9_restAPIwithMongoose/index.js
+++ Session_9_restAPIwithMongoose/index.js
@@ -1,12 +1,14 @@
-const express=require('express')
-const dotenv=require('dotenv');
+const express = require('express')
+const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cors = require('cors') //import cores
 
 dotenv.config() // config of dot env file using for data hidding
 const app = express();
 app.use(express.json())
+app.use(cors())//checking a cors
 connectDB();
 
-app.use("/api/user",require('./routes/userRoute'))
-const PORT =process.env.PORT;// reading the file in env file
-app.listen(PORT,()=>console.log(`Application is listen in ${PORT}`));
+app.use("/api/user", require('./routes/userRoute'))
+const PORT = process.env.PORT;// reading the file in env file
+app.listen(PORT, () => console.log(`Application is listen in ${PORT}`));
